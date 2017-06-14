96c2777b484587e24601e105980f010eeecc8e4b
mveillon <matthieu.veillon@gmail.com>
2017-06-13 20:29:11 -0400

    first commit

---
+++ Goldiluck.js
@@ -0,0 +1,8 @@
+const story = {
+    const title = document.getElementById('title');
+    title.innerText = 'Forest, do you want to enter'
+    const button = document.createElement('button1');
+    button1.innerText = 'Enter';
+    const button = document.createElement('button2');
+    button2.innerText = 'Dont Enter';
+}
